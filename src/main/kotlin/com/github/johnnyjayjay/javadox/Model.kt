ce2854cc113072058fdb9149cd3cd85de2ec2c0f
JohnnyJayJay <johnnyjayjay02@gmail.com>
2020-03-31 21:02:35 +0200

    Add common Documented interface

--- src/main/kotlin/com/github/johnnyjayjay/javadox/Model.kt
+++ src/main/kotlin/com/github/johnnyjayjay/javadox/Model.kt
@@ -1,28 +1,37 @@
 package com.github.johnnyjayjay.javadox
 
+interface Documented {
+  val name: String
+  val uri: String
+  val declaration: String
+  val description: String
+  val deprecation: String?
+  val tags: List<Pair<String, List<String>>>
+}
+
 data class DocumentedType(
     val `package`: String,
-    val uri: String,
+    override val uri: String,
     val type: String,
-    val name: String,
+    override val name: String,
     val inheritance: List<String>,
     val topTags: List<Pair<String, List<String>>>,
-    val declaration: String,
-    val description: String,
-    val deprecation: String?,
-    val bottomTags: List<Pair<String, List<String>>>,
+    override val declaration: String,
+    override val description: String,
+    override val deprecation: String?,
+    override val tags: List<Pair<String, List<String>>>,
     val inheritedMethods: Map<String, List<String>>,
     val enumConstants: List<DocumentedMember>,
     val fields: List<DocumentedMember>,
     val constructors: List<DocumentedMember>,
     val methods: List<DocumentedMember>
-)
+) : Documented
 
 data class DocumentedMember(
-    val uri: String,
-    val name: String,
-    val declaration: String,
-    val description: String,
-    val deprecation: String?,
-    val tags: List<Pair<String, List<String>>>
-)
+    override val uri: String,
+    override val name: String,
+    override val declaration: String,
+    override val description: String,
+    override val deprecation: String?,
+    override val tags: List<Pair<String, List<String>>>
+) : Documented
